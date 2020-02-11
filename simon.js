47f435405e1e450817bd1f284d663cbd2abf57d3
Simon Le <a01042025@se14l0124-22.ad.bcit.ca>
2020-02-11 10:34:18 -0800

    made changes

--- simon.js
+++ simon.js
@@ -1,2 +1,2 @@
 // this is my file 
-// this is the first changes can somebody see this ? sorry i did made changes in sunny file =))
+// this is the first changes can somebody     see this ? sorry i did made changes in sunny file =))
